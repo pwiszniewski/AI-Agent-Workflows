225fc24025d9f79f00563dcef610b48db64cadbe
Przemyslaw Wiszniewski <przemyslaw.wiszniewski@itr.lukasiewicz.gov.pl>
2026-07-28 12:12:42 +0200

    .

--- my_code/ch04/exercise4_handoffs_graph.py
+++ my_code/ch04/exercise4_handoffs_graph.py
@@ -13,12 +13,18 @@ from dotenv import load_dotenv
 load_dotenv()                       # konieczne żeby widział klucz API !!!
 
 SANDBOX = os.path.dirname(os.path.abspath(__file__))
-SCRIPT = Path(__file__).with_name("01_research_tools_mcp_server.py").resolve()
+
+SCRIPT = Path(__file__).with_name(
+    "01_research_tools_mcp_server.py").resolve()
 
 client = AsyncOpenAI(
     api_key=os.getenv("GEMINI_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
+model_name = 'gemini-3.1-flash-lite'
+# model_name = 'gemini-3-flash-preview'
+# model_name = 'gemini-3.6-flash'
+
 
 from pydantic import BaseModel
 from typing import List
@@ -61,10 +67,10 @@ research_agent = Agent(
     instructions="""
         You are a research assistant.
         Your role is to find research sources.
+        Always hand off to planning agent
         """,
     model=OpenAIChatCompletionsModel(
-            # model="gemini-3.5-flash",
-            model="gemini-3.1-flash-lite",
+            model=model_name,
             openai_client=client,
         ),
     output_type=ResearchSourcesModel
@@ -74,13 +80,28 @@ thinking_agent = Agent(
     instructions="""
         You are a research assistant.
         Your role is to plan the research.
+        Always hand off to filesystem agent
         """,
     model=OpenAIChatCompletionsModel(
-            # model="gemini-3.5-flash",
-            model="gemini-3.1-flash-lite",
+            model=model_name,
             openai_client=client,
         ),
 )
+
+filesystem_agent = Agent(
+    name="Filesystem Agent",
+    instructions="""
+    You are a filesystem assistant.
+    Your role is to write the output as a text file to 'out_exercise3.txt'
+    Never make up or invent any ouput.
+    """,
+    model=OpenAIChatCompletionsModel(
+                model=model_name,
+                openai_client=client,
+            ),
+)
+
+from agents.extensions.visualization import draw_graph
 
 async def main():
     async with (
@@ -88,29 +109,28 @@ async def main():
         servers[1] as thinking_srv,
         servers[2] as fs_srv,
     ):
+
+        research_agent.mcp_servers = [research_srv]
+        research_agent.handoffs = [thinking_agent]
+        thinking_agent.mcp_servers = [thinking_srv]
+        thinking_agent.handoffs = [filesystem_agent]
+        filesystem_agent.mcp_servers = [fs_srv]
+
         goal = """
             Produce a research plan to find the book:
             'The Hitchhiker's Guide to the Galaxy'
             """
-        print("Running...", goal)
-        research_agent.mcp_servers = [research_srv]
-        result = await Runner.run(research_agent, goal)
-        research_sources = result.final_output.research_sources
-        print('research agent:', research_sources)
-        print('len of research plan:', len(research_sources))
 
-        if research_sources and len(research_sources) > 0:
-            thinking_agent.mcp_servers = [thinking_srv]
-            agent_input = dict(
-                 research_sources=research_sources,
-                 goal=goal,
+        # rysowanie wykresu
+        draw_graph(research_agent).render(view=True) # reasearch agent nie wywołuje bezpośrednio filesystem agent, ale przez thinking agenta
+        
+        print("Running...", goal)
+        result = await Runner.run(
+            research_agent,
+            goal, 
+            max_turns=25,
             )
-            result = await Runner.run(thinking_agent, str(agent_input)) 
-            print('################################################################################')
-            print('thinking agent:', result.final_output)
-        else:
-             print('no research plan - aborted !')
-             exit()
+        print(result.final_output)
 
 if __name__ == "__main__":
     asyncio.run(main())
